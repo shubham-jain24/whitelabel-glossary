74de4797ea7b3c253d9d52ac5f38a0be83ac7380
ShubhamT JainT <shubhjain24999@gmail.com>
2024-07-07 18:31:53 +0000

    Updated Vercel config

--- src/Component/DisplayPostList/DisplayPostList.tsx
+++ src/Component/DisplayPostList/DisplayPostList.tsx
@@ -4,65 +4,68 @@ import DisplayBlog, { BlogPostData } from "../DisplayBlog/DisplayBlog";
 import ThemeContext from "../../Utils/ThemeContext";
 
 type Props = {
-    whitelabelId: string;
-}
+  whitelabelId: string;
+};
 
 const DisplayPostList: React.FC<Props> = (props) => {
-    const themeContext = React.useContext(ThemeContext);
+  const themeContext = React.useContext(ThemeContext);
 
-    const [postList, setPostList] = useState([] as BlogPostData[]);
+  const [postList, setPostList] = useState([] as BlogPostData[]);
 
-    const fetchPostList = () => {
-        const body = {
-            "whitelabelId": props.whitelabelId,
-        }
-        axios.post('/Prod/GetPost', (JSON.stringify(body)), {
-            headers: {
-                'Content-Type': 'text/plain'
-            }
-        })
-        .then(response => {
-            const blogData: BlogPostData[] = response?.data?.body?.data
-            setPostList(blogData)
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
+  const fetchPostList = () => {
+    const body = {
+      whitelabelId: props.whitelabelId,
+    };
+    axios
+      .post(
+        "https://6ijq8h7rq7.execute-api.eu-north-1.amazonaws.com/Prod/CreatePost",
+        JSON.stringify(body),
+        {
+          headers: {
+            "Content-Type": "text/plain",
+          },
+        },
+      )
+      .then((response) => {
+        const blogData: BlogPostData[] = response?.data?.body?.data;
+        setPostList(blogData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    useEffect(()=> {
-        fetchPostList();
-    }, [])
+  useEffect(() => {
+    fetchPostList();
+  }, []);
 
-
-    const renderPostList = () => {
-        return (
-            <div>
-                {postList.map((post) => (
-                    <DisplayBlog key={post.PostId} {...post} />) )}
-            </div>
-        );
-    }
-
-    const renderLoading = () => {
-        return (
-            <>
-                <div className={`w-8/12 m-auto ${themeContext.bgFifth} rounded-lg my-8 shadow-2xl min-h-60 text-lg mb-14 opacity-45`}>
-                </div>
-                <div className={`w-8/12 m-auto ${themeContext.bgFifth} rounded-lg my-8 shadow-2xl min-h-60 h-1/4 text-lg  mb-14 opacity-50`}>
-                </div>
-                <div className={`w-8/12 m-auto ${themeContext.bgFifth} rounded-lg my-8 min-h-60 shadow-2xl h-1/4 text-lg  mb-14 opacity-40`}>
-                </div>
-            </>
-        )
-    }
-
+  const renderPostList = () => {
     return (
-        <>
-        {postList.length ? renderPostList() : renderLoading()}
-        </>
-    )
+      <div>
+        {postList.map((post) => (
+          <DisplayBlog key={post.PostId} {...post} />
+        ))}
+      </div>
+    );
+  };
 
+  const renderLoading = () => {
+    return (
+      <>
+        <div
+          className={`w-8/12 m-auto ${themeContext.bgFifth} rounded-lg my-8 shadow-2xl min-h-60 text-lg mb-14 opacity-45`}
+        ></div>
+        <div
+          className={`w-8/12 m-auto ${themeContext.bgFifth} rounded-lg my-8 shadow-2xl min-h-60 h-1/4 text-lg  mb-14 opacity-50`}
+        ></div>
+        <div
+          className={`w-8/12 m-auto ${themeContext.bgFifth} rounded-lg my-8 min-h-60 shadow-2xl h-1/4 text-lg  mb-14 opacity-40`}
+        ></div>
+      </>
+    );
+  };
+
+  return <>{postList.length ? renderPostList() : renderLoading()}</>;
 };
 
 export default DisplayPostList;
