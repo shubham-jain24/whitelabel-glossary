76e5c4171c3f9e4e2a4cf85a77b2874844e0f741
ShubhamT JainT <shubhjain24999@gmail.com>
2024-07-13 07:15:16 +0000

    Updated dist wihout hash

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      server: {
-        proxy: {
-          '/Prod': 'https://6ijq8h7rq7.execute-api.eu-north-1.amazonaws.com',
-          '/Prod/Login': 'https://6ijq8h7rq7.execute-api.eu-north-1.amazonaws.com'
-        }
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
       },
-      plugins: [react()],
-  });
-  
+    },
+  },
+  server: {
+    proxy: {
+      "/Prod": "https://6ijq8h7rq7.execute-api.eu-north-1.amazonaws.com",
+      "/Prod/Login": "https://6ijq8h7rq7.execute-api.eu-north-1.amazonaws.com",
+    },
+  },
+  plugins: [react()],
+});
